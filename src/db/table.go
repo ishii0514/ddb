4ca96eced7b06a576da3520849ecb9c4667db028
Yosuke Ishii <ishii0514@gmail.com>
2012-09-20 01:38:45 +0900

    リファクタ

--- src/db/table.go
+++ src/db/table.go
@@ -2,8 +2,8 @@ package db
 
 import (
 )
-//TODO カラムの生成
-//TODO テーブルの生成
+//TODO カラムの生成 ファクトリメソッド
+//TODO テーブルの生成 ファクトリメソッド
 //TODO Insertのテスト
 //テーブル
 type Table struct{
@@ -17,9 +17,11 @@ func (p *Table) Name() string { return p.name}
 
 //データ数
 func (p *Table) DataCount() ROWNUM { return p.datacount}
+
+
 /**
 * データ挿入
-* カラム数より少ない場合、変換できない値が入ってる場合、0で埋められる。
+* カラム数より少ない場合、変換できない値が入ってる場合、INVALID_VALUEで埋められる。
 * カラム数より多い列のデータは無視される。
 */
 func (p *Table) Insert(values []string) {
@@ -35,10 +37,9 @@ func (p *Table) Insert(values []string) {
 
 //投入データを決定する。
 func getInsertVaue(columnIndex int,insertValues *[]string) string{
-	insertValue := INVALID_VALUE
 	//カラムインデックスの方が大きい場合
 	if columnIndex >= len(*insertValues){
-		return insertValue
+		return INVALID_VALUE
 	}
 	return (*insertValues)[columnIndex]
 }
