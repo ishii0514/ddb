f063b7efcc5b13caa2152f98e03a685b2cf7b759
Yosuke Ishii <ishii0514@gmail.com>
2012-09-27 23:12:39 +0900

    bechmark試行

--- src/db/dataInteger_test_.go
+++ src/db/dataInteger_test_.go
@@ -95,3 +95,9 @@ func TestArrayIntegerDelete(t *testing.T) {
         t.Error("illegal no delete count.")
     }
 }
+func BenchmarkArrayIntegerInsert(b *testing.B) {
+    var data1  = ArrayInteger{}
+    for i := 0; i< b.N; i++ {
+    	data1.Insert(Integer(i))
+    }
+}
