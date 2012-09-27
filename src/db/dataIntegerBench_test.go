39e5907f3776fadae0a4db66251516f94505b6e4
ishii0514 <ishii0514@gmail.com>
2012-09-28 00:08:54 +0900

    search,deleteのベンチマーク

--- src/db/dataIntegerBench_test.go
+++ src/db/dataIntegerBench_test.go
@@ -2,11 +2,55 @@ package db
 
 import (
     "testing"
+    "math/rand"
 )
 
+//データ件数
+var DATA_CNT int = 10000000
+
+//Insertの計測
 func BenchmarkArrayIntegerInsert(b *testing.B) {
     var data1  = ArrayInteger{}
     for i:=0;i<b.N;i++ {
-        data1.Insert(1)
+        data1.Insert(Integer(i))
     }
+}
+
+//Searchの計測
+func BenchmarkArrayIntegerSearch(b *testing.B) {
+    b.StopTimer()
+    var data1  = createDataArray(DATA_CNT)
+    var seachValues = randumValues(b.N,DATA_CNT)
+    b.StartTimer()
+    for _, v := range seachValues {
+        data1.Search(v)
+    }
+}
+
+//Deleteの計測
+func BenchmarkArrayIntegerDelete(b *testing.B) {
+    b.StopTimer()
+    var data1  = createDataArray(DATA_CNT)
+    var seachValues = randumValues(b.N,DATA_CNT)
+    b.StartTimer()
+    for _, v := range seachValues {
+        data1.Delete(v)
+    }
+}
+
+//指定したデータ件数のArrayを生成
+func createDataArray(datanumber int) ArrayInteger {
+    var dataarray  = ArrayInteger{}
+    for i:=0;i<datanumber;i++ {
+        dataarray.Insert(Integer(i))
+    }
+    return dataarray
+}
+//ランダムなIntegerを生成
+func randumValues(roopCnt int,upperNumber int) []Integer{
+    var seachValues = []Integer{}
+    for i:=0;i<roopCnt;i++ {
+        seachValues = append(seachValues,Integer(rand.Intn(upperNumber)))
+    }
+    return seachValues
 }
