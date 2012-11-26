0e198c999c96ab98ba2945f9017081261cc7f90c
Yosuke Ishii <ishii0514@gmail.com>
2012-11-27 01:03:54 +0900

    ttree

---
+++ src/db/ttree.go
@@ -0,0 +1,95 @@
+package db
+
+import (
+
+)
+
+//Tteeデータ構造
+type TtreeInteger struct{
+    rootNode *tnode
+    dataCount ROWNUM
+    rowid ROWNUM
+}
+//TtreeIntegerコンストラクタ
+func CreateTtree(t int) TtreeInteger {
+    return TtreeInteger{rootNode:createTnode(t),dataCount:0,rowid:0}
+}
+
+//データ数
+func(p *TtreeInteger) DataCount() ROWNUM{
+    return p.dataCount
+}
+
+//行指定（ダミー）
+func(p *TtreeInteger) Get(row ROWNUM) (Integer,error){
+    return 0,nil
+}
+//挿入
+func(p *TtreeInteger) Insert(insertValue Integer) ROWNUM{
+	//TODO tnode.insert
+    p.dataCount += 1
+    p.rowid += 1
+    return ROWNUM(1)
+}
+
+
+//Tツリーのノード
+type tnode struct{
+    //ノードサイズ
+    t int
+    //データ数
+    dataCount int    
+    //値
+    values []nodeValue
+    
+    //parentノード
+    parentNode  *tnode
+    //leftノード
+    leftNode  *tnode
+    //rightノード
+    rightNode  *tnode
+}
+
+//Tノードコンストラクタ
+func createTnode(t int) *tnode{
+    newNode := new(tnode)
+    newNode.values = make([]nodeValue,t)
+    newNode.t = t
+    newNode.dataCount = 0
+    newNode.parentNode = nil
+    newNode.leftNode = nil
+    newNode.rightNode = nil
+    return newNode
+}
+//Tnodeインサート
+//TODO test
+func(p *tnode) Insert(insertValue Integer,row ROWNUM) {
+	if p.dataCount == 0 {
+		//TODO データ0件
+		//p.insertValue(0,nodeValue{insertValue,[]ROWNUM{row}})
+		return
+	}
+	//TODO 子ノードがない場合
+	if insertValue > p.maxValue() && p.leftNode != nil {
+		p.leftNode.Insert(insertValue,row)
+	} else if insertValue < p.minValue() && p.rightNode != nil {
+		p.rightNode.Insert(insertValue,row)
+	} else{
+		//TODO 本ノードでの処理
+	}
+}
+func(p *tnode) maxValue() Integer{
+	return p.values[p.dataCount-1].key
+}
+func(p *tnode) minValue() Integer{
+	return p.values[0].key
+}
+
+//ノード内に値を挿入する
+func(p *tnode) insertValue(insertPos int,insertNodeValue nodeValue) {
+    for i:= p.dataCount;i > insertPos;i-- {
+        p.values[i] = p.values[i-1]
+    }
+    p.values[insertPos] = insertNodeValue
+    p.dataCount += 1
+}
