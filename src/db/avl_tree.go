c09305329028dfa726b6bffccde1e21e30696bbd
Toshitaka Adachi <adatch.t@gmail.com>
2013-01-18 09:35:17 +0900

    implment select, insert

--- src/db/avl_tree.go
+++ src/db/avl_tree.go
@@ -1,35 +1,110 @@
 package db
 
 import (
-
+	"fmt"
 )
 
-// implements CompareTo(NodeValue)
-type NodeValue nodeValue
+// implements CompareTo(Elem)
+type Elem nodeValue
 
 // compares two keys. 
 // returns negative if p.key < value.key, 
 // positive if p.key > value.key,
 // or 0 if p.key == value.key
-func (p *NodeValue) CompareTo(value NodeValue) int {
+func (p *Elem) CompareTo(value Elem) int {
 	return int(p.key - value.key)
+}
+
+// return true or false
+func AreEqual(s1, s2 []ROWNUM) bool {
+	if len(s1) != len(s2) {
+		return false
+	}
+	for i := 0; i < len(s1); i++ {
+		if s1[i] != s2[i] {
+			return false
+		}
+	}
+	return true
 }
 
 // node of AVLTree
 type AVLNode struct {
-	data NodeValue
+	data   Elem
 	height int
 	parent *AVLNode
-	left  *AVLNode
-	right *AVLNode
+	left   *AVLNode
+	right  *AVLNode
 }
 
 // makes a new node with given value and parent, and with nil child links.
+// NOTE: the height of leaf node is 1.
 // param: value
 // param: parent
-func CreateAVLNode(data NodeValue, parent *AVLNode) *AVLNode {
-	return &AVLNode{data, 0, parent, nil, nil}
+func CreateAVLNode(data Elem, parent *AVLNode) *AVLNode {
+	// re-calculate ancestor's height
+	height := 2
+	ancestor := parent
+	for ancestor != nil && height > ancestor.height {
+		ancestor.height = height
+		height = height + 1
+		ancestor = ancestor.parent
+	}
+	return &AVLNode{data, 1, parent, nil, nil}
 }
+
+// returns true of false
+func (this *AVLNode) Equals(obj *AVLNode) bool {
+	if this == obj {
+		return true
+	}
+	if obj == nil {
+		return false
+	}
+	other := obj
+	if this.Key() != other.Key() {
+		return false
+	}
+	if this.height != other.height {
+		return false
+	}
+	if this.parent == nil {
+		if other.parent != nil {
+			return false
+		}
+	} else if other.parent == nil || this.parent.Key() != other.parent.Key() {
+		return false
+	}
+	if this.left == nil {
+		if other.left != nil {
+			return false
+		}
+	} else if !this.left.Equals(other.left) {
+		return false
+	}
+	if this.right == nil {
+		if other.right != nil {
+			return false
+		}
+	} else if !this.right.Equals(other.right) {
+		return false
+	}
+	if this.Value() == nil {
+		if other.Value() != nil {
+			return false
+		}
+	} else if !AreEqual(this.Value(), other.Value()) {
+		return false
+	}
+	return true
+}
+
+func (p *AVLNode) String() string {
+	return fmt.Sprintf("Node{key:%v value:%v height:%v left:%v right:%v}",
+		p.Key(), p.Value(), p.height, p.left, p.right)
+}
+
+// Getters
 
 // returns the key of data
 func (p *AVLNode) Key() Integer {
@@ -41,10 +116,24 @@ func (p *AVLNode) Value() []ROWNUM {
 	return p.data.rows
 }
 
-// appends row value to rows
-func (p *AVLNode) AppendValue(row ROWNUM) *AVLNode {
-	p.data.rows = append(p.data.rows, row)
-	return p
+// returns height
+func (p *AVLNode) Height() int {
+	return p.height
+}
+
+// returns parent
+func (p *AVLNode) Parent() *AVLNode {
+	return p.parent
+}
+
+// returns left
+func (p *AVLNode) Left() *AVLNode {
+	return p.left
+}
+
+// returns right
+func (p *AVLNode) Right() *AVLNode {
+	return p.right
 }
 
 // AVL Tree
@@ -52,7 +141,7 @@ type AVLTreeInteger struct {
 	// root node
 	root *AVLNode
 	// number of entries in the tree
-	dataCount ROWNUM
+	data_count ROWNUM
 }
 
 // creates a new empty AVL Tree. 
@@ -62,7 +151,7 @@ func CreateAVLTreeInteger() *AVLTreeInteger {
 
 // returns the number of entries
 func (p *AVLTreeInteger) DataCount() ROWNUM {
-	return p.dataCount
+	return p.data_count
 }
 
 // returns key associated with given value(represents rowid)
@@ -89,13 +178,14 @@ func (p *AVLTreeInteger) Search(key Integer) []ROWNUM {
 
 // returns entry for given key, or nil if the map does not contain
 // an entry for the key.
-func(p *AVLTreeInteger) GetEntry(key Integer) *AVLNode {
+func (p *AVLTreeInteger) GetEntry(key Integer) *AVLNode {
 	node := p.root
-	for ; node != nil; {
+	for node != nil {
 		cmp := p.Compare(key, node.Key())
 		if cmp == 0 {
 			return node
-		} else if cmp < 0 {
+		}
+		if cmp < 0 {
 			node = node.left
 		} else {
 			node = node.right
@@ -109,28 +199,170 @@ func(p *AVLTreeInteger) GetEntry(key Integer) *AVLNode {
 // If the map previously contained a mapping for this key, 
 // append the newest value and returned.
 func (p *AVLTreeInteger) Insert(key Integer) ROWNUM {
-	if p.InsertKey(key) != nil {
+	added := false
+	p.root, added = p.InsertKey(key)
+	if added {
+		p.root = p.resetHeight(p.root)
+		p.data_count = p.data_count + 1
 		return ROWNUM(1)
 	}
 	return ROWNUM(0)
 }
 
-func (p *AVLTreeInteger) InsertKey(key Integer) *AVLNode {
-	return p.insert(key, p.dataCount + 1)
+func (p *AVLTreeInteger) InsertKey(key Integer) (*AVLNode, bool) {
+	return p.insert(p.root, nil, key, p.data_count+1)
 }
 
-func (p *AVLTreeInteger) insert(key Integer, row ROWNUM) *AVLNode {
-	t := p.root
-	if t == nil {
-		p.dataCount++
-		p.root = p.Construct(NodeValue{key, []ROWNUM{row}}, nil)
-		return p.root
+// return new element associated with given key, or nil
+func (p *AVLTreeInteger) insert(node, parent *AVLNode, key Integer, row ROWNUM) (*AVLNode, bool) {
+	if node == nil {
+		return p.Construct(Elem{key, []ROWNUM{row}}, parent), true
 	}
-	return nil
+
+	cmp := p.Compare(key, node.Key())
+	if cmp == 0 {
+		node = p.AppendValue(node, row)
+		return node, true
+	}
+	added := true
+	if cmp < 0 {
+		node.left, added = p.insert(node.left, node, key, row)
+		if node.left != nil && node.left.parent == nil {
+			node.left.parent = node
+		}
+		if added {
+			return p.Rebalance(node), added
+		}
+	} else {
+		node.right, added = p.insert(node.right, node, key, row)
+		if node.right != nil && node.right.parent == nil {
+			node.right.parent = node
+		}
+		if added {
+			return p.Rebalance(node), added
+		}
+	}
+	// unfortunately break loop
+	return nil, false
 }
 
 // Construct a node in the tree.
-func (p *AVLTreeInteger) Construct(data NodeValue, parent *AVLNode) *AVLNode {
+func (p *AVLTreeInteger) Construct(data Elem, parent *AVLNode) *AVLNode {
 	return CreateAVLNode(data, parent)
 }
 
+// Rebalance subtree
+func (p *AVLTreeInteger) Rebalance(subroot *AVLNode) *AVLNode {
+	newroot := subroot
+	if subroot != nil {
+		// calculate balance factor
+		factor := p.Height(subroot.left) - p.Height(subroot.right)
+		if factor < -1 {
+			factor = p.Height(subroot.right.left) - p.Height(subroot.right.right)
+			if factor == -1 {
+				// RR case
+			} else if factor == 1 {
+				// RL case
+				p.rotateRight(subroot.right)
+			}
+			newroot = p.rotateLeft(subroot)
+		} else if factor > 1 {
+			factor = p.Height(subroot.left.left) - p.Height(subroot.left.right)
+			if factor == 1 {
+				// LL case
+			} else if factor == -1 {
+				// LR case
+				p.rotateLeft(subroot.left)
+			}
+			newroot = p.rotateRight(subroot)
+		}
+	}
+	return newroot
+}
+
+// appends row value to rows
+func (p *AVLTreeInteger) AppendValue(node *AVLNode, row ROWNUM) *AVLNode {
+	if node != nil {
+		node.data.rows = append(node.data.rows, row)
+	}
+	return node
+}
+
+func (p *AVLTreeInteger) Height(node *AVLNode) int {
+	height := 0
+	if node != nil {
+		height = node.height
+	}
+	return height
+}
+
+// rotates left around node
+func (p *AVLTreeInteger) rotateLeft(node *AVLNode) *AVLNode {
+	right := node.right
+	node.right = right.left
+	if right.left != nil {
+		right.left.parent = node
+	}
+	right.parent = node.parent
+	if node.parent == nil {
+		p.root = right
+	} else if node.parent.left == node {
+		node.parent.left = right
+	} else {
+		node.parent.right = right
+	}
+	right.left = node
+	node.parent = right
+	return right
+}
+
+// rotates right around node
+func (p *AVLTreeInteger) rotateRight(node *AVLNode) *AVLNode {
+	left := node.left
+	node.left = left.right
+	if left.right != nil {
+		left.right.parent = node
+	}
+	left.parent = node.parent
+	if node.parent == nil {
+		p.root = left
+	} else if node.parent.right == node {
+		node.parent.right = left
+	} else {
+		node.parent.left = left
+	}
+	left.right = node
+	node.parent = left
+	return left
+}
+
+func (p *AVLTreeInteger) resetHeight(node *AVLNode) *AVLNode {
+	if node != nil {
+		node.left = p.resetHeight(node.left)
+		node.right = p.resetHeight(node.right)
+		h_left, h_right := p.Height(node.left), p.Height(node.right)
+		if h_left > h_right {
+			node.height = h_left + 1
+		} else {
+			node.height = h_right + 1
+		}
+	}
+	return node
+}
+
+// Delete
+// return deleted row count
+func (p *AVLTreeInteger) Delete(key Integer) ROWNUM {
+	node := p.GetEntry(key)
+	if node == nil {
+		return 0
+	}
+	deleted_count := ROWNUM(len(node.Value()))
+	p.remove(node)
+	p.data_count = p.data_count - deleted_count
+	return deleted_count
+}
+
+// remove given node from tree
+func (p *AVLTreeInteger) remove(node *AVLNode) {
+}
--- src/db/avl_tree_test.go
+++ src/db/avl_tree_test.go
@@ -1,10 +1,219 @@
 package db
 
 import (
-    "testing"
+	"testing"
 )
 
-func TestAVLNodeCreate(t *testing.T) {
+type TestData struct {
+	actual []ROWNUM
+	expect []ROWNUM
+}
 
+func TestAVLTreeSearch(t *testing.T) {
+	// create test tree
+	tree := CreateAVLTreeInteger()
+
+	node1 := CreateAVLNode(Elem{12, []ROWNUM{10}}, nil)
+	node2 := CreateAVLNode(Elem{1, []ROWNUM{20, 30}}, node1)
+	node3 := CreateAVLNode(Elem{20, []ROWNUM{40, 50}}, node1)
+	node4 := CreateAVLNode(Elem{15, []ROWNUM{60, 70, 80}}, node3)
+
+	node3.left = node4
+	node1.left = node2
+	node1.right = node3
+
+	tree.root = node1
+
+	// if the key exists, return the value associated with given key
+	// else, return []ROWNUM{}	
+	data := []TestData{
+		TestData{tree.Search(12), []ROWNUM{10}},
+		TestData{tree.Search(1), []ROWNUM{20, 30}},
+		TestData{tree.Search(20), []ROWNUM{40, 50}},
+		TestData{tree.Search(15), []ROWNUM{60, 70, 80}},
+		TestData{tree.Search(35), []ROWNUM{}},
+	}
+
+	for _, v := range data {
+		if !AreEqual(v.actual, v.expect) {
+			t.Errorf("expect: %v, actual: %v", v.expect, v.actual)
+		}
+	}
+}
+
+func TestAVLTreeInsert(t *testing.T) {
+	var tree *AVLTreeInteger = nil
+	var act_row, exp_row ROWNUM
+	var act_node, exp_node *AVLNode
+	var act_cnt, exp_cnt ROWNUM
+	exp_row = 1
+
+	// if empty tree inserts the key, the root is created
+	tree = CreateAVLTreeInteger()
+	act_row = tree.Insert(20)
+	act_node = tree.root
+	exp_node = &AVLNode{Elem{20, []ROWNUM{1}}, 1, nil, nil, nil}
+	if exp_row != act_row {
+		t.Errorf("expect: %v, actual: %v", exp_row, act_row)
+	}
+	if !act_node.Equals(exp_node) {
+		t.Errorf("expect: %v, actual: %v", exp_node, act_node)
+	}
+	act_cnt, exp_cnt = 1, tree.DataCount()
+	if act_cnt != exp_cnt {
+		t.Errorf("\nexpect: %v, \nactual: %v", exp_cnt, act_cnt)
+	}
+
+	// add to left
+	tree = CreateAVLTreeInteger()
+	act_row = tree.Insert(20)
+	act_row = tree.Insert(12)
+	act_node = tree.root
+	exp_node = &AVLNode{Elem{20, []ROWNUM{1}}, 2, nil, nil, nil}
+	exp_node.left = &AVLNode{Elem{12, []ROWNUM{2}}, 1, exp_node, nil, nil}
+	if exp_row != act_row {
+		t.Errorf("expect: %v, actual: %v", exp_row, act_row)
+	}
+	if !act_node.Equals(exp_node) {
+		t.Errorf("\nexpect: %v, \nactual: %v", exp_node, act_node)
+	}
+	act_cnt, exp_cnt = 2, tree.DataCount()
+	if act_cnt != exp_cnt {
+		t.Errorf("\nexpect: %v, \nactual: %v", exp_cnt, act_cnt)
+	}
+
+	// add to right
+	tree = CreateAVLTreeInteger()
+	act_row = tree.Insert(20)
+	act_row = tree.Insert(30)
+	act_node = tree.root
+	exp_node = &AVLNode{Elem{20, []ROWNUM{1}}, 2, nil, nil, nil}
+	exp_node.right = &AVLNode{Elem{30, []ROWNUM{2}}, 1, exp_node, nil, nil}
+	if exp_row != act_row {
+		t.Errorf("expect: %v, actual: %v", exp_row, act_row)
+	}
+	if !act_node.Equals(exp_node) {
+		t.Errorf("\nexpect: %v, \nactual: %v", exp_node, act_node)
+	}
+	act_cnt, exp_cnt = 2, tree.DataCount()
+	if act_cnt != exp_cnt {
+		t.Errorf("\nexpect: %v, \nactual: %v", exp_cnt, act_cnt)
+	}
+	
+	// duplicate
+	tree = CreateAVLTreeInteger()
+	act_row = tree.Insert(20)
+	act_row = tree.Insert(30)
+	act_row = tree.Insert(30)
+	act_node = tree.root
+	exp_node = &AVLNode{Elem{20, []ROWNUM{1}}, 2, nil, nil, nil}
+	exp_node.right = &AVLNode{Elem{30, []ROWNUM{2, 3}}, 1, exp_node, nil, nil}
+	if exp_row != act_row {
+		t.Errorf("expect: %v, actual: %v", exp_row, act_row)
+	}
+	if !act_node.Equals(exp_node) {
+		t.Errorf("\nexpect: %v, \nactual: %v", exp_node, act_node)
+	}
+	act_cnt, exp_cnt = 3, tree.DataCount()
+	if act_cnt != exp_cnt {
+		t.Errorf("\nexpect: %v, \nactual: %v", exp_cnt, act_cnt)
+	}
+	tree = CreateAVLTreeInteger()
+	act_row = tree.Insert(20)
+	act_row = tree.Insert(10)
+	act_row = tree.Insert(20)
+	act_node = tree.root
+	exp_node = &AVLNode{Elem{20, []ROWNUM{1, 3}}, 2, nil, nil, nil}
+	exp_node.left = &AVLNode{Elem{10, []ROWNUM{2}}, 1, exp_node, nil, nil}
+	if exp_row != act_row {
+		t.Errorf("expect: %v, actual: %v", exp_row, act_row)
+	}
+	if !act_node.Equals(exp_node) {
+		t.Errorf("\nexpect: %v, \nactual: %v", exp_node, act_node)
+	}
+	act_cnt, exp_cnt = 3, tree.DataCount()
+	if act_cnt != exp_cnt {
+		t.Errorf("\nexpect: %v, \nactual: %v", exp_cnt, act_cnt)
+	}
+	
+	// RR case
+	tree = CreateAVLTreeInteger()
+	act_row = tree.Insert(20)
+	act_row = tree.Insert(30)
+	act_row = tree.Insert(40)
+	act_node = tree.root
+	exp_node = &AVLNode{Elem{30, []ROWNUM{2}}, 2, nil, nil, nil}
+	exp_node.right = &AVLNode{Elem{40, []ROWNUM{3}}, 1, exp_node, nil, nil}
+	exp_node.left = &AVLNode{Elem{20, []ROWNUM{1}}, 1, exp_node, nil, nil}
+	if exp_row != act_row {
+		t.Errorf("expect: %v, actual: %v", exp_row, act_row)
+	}
+	if !act_node.Equals(exp_node) {
+		t.Errorf("\nexpect: %v, \nactual: %v", exp_node, act_node)
+	}
+	act_cnt, exp_cnt = 3, tree.DataCount()
+	if act_cnt != exp_cnt {
+		t.Errorf("\nexpect: %v, \nactual: %v", exp_cnt, act_cnt)
+	}
+
+	// RL rotation
+	tree = CreateAVLTreeInteger()
+	act_row = tree.Insert(20)
+	act_row = tree.Insert(40)
+	act_row = tree.Insert(30)
+	act_node = tree.root
+	exp_node = &AVLNode{Elem{30, []ROWNUM{3}}, 2, nil, nil, nil}
+	exp_node.right = &AVLNode{Elem{40, []ROWNUM{2}}, 1, exp_node, nil, nil}
+	exp_node.left = &AVLNode{Elem{20, []ROWNUM{1}}, 1, exp_node, nil, nil}
+	if exp_row != act_row {
+		t.Errorf("expect: %v, actual: %v", exp_row, act_row)
+	}
+	if !act_node.Equals(exp_node) {
+		t.Errorf("\nexpect: %v, \nactual: %v", exp_node, act_node)
+	}
+	act_cnt, exp_cnt = 3, tree.DataCount()
+	if act_cnt != exp_cnt {
+		t.Errorf("\nexpect: %v, \nactual: %v", exp_cnt, act_cnt)
+	}
+
+	// LL rotation
+	tree = CreateAVLTreeInteger()
+	act_row = tree.Insert(20)
+	act_row = tree.Insert(10)
+	act_row = tree.Insert(5)
+	act_node = tree.root
+	exp_node = &AVLNode{Elem{10, []ROWNUM{2}}, 2, nil, nil, nil}
+	exp_node.right = &AVLNode{Elem{20, []ROWNUM{1}}, 1, exp_node, nil, nil}
+	exp_node.left = &AVLNode{Elem{5, []ROWNUM{3}}, 1, exp_node, nil, nil}
+	if exp_row != act_row {
+		t.Errorf("expect: %v, actual: %v", exp_row, act_row)
+	}
+	if !act_node.Equals(exp_node) {
+		t.Errorf("\nexpect: %v, \nactual: %v", exp_node, act_node)
+	}
+	act_cnt, exp_cnt = 3, tree.DataCount()
+	if act_cnt != exp_cnt {
+		t.Errorf("\nexpect: %v, \nactual: %v", exp_cnt, act_cnt)
+	}
+	
+	// LR rotation
+	tree = CreateAVLTreeInteger()
+	act_row = tree.Insert(20)
+	act_row = tree.Insert(5)
+	act_row = tree.Insert(10)
+	act_node = tree.root
+	exp_node = &AVLNode{Elem{10, []ROWNUM{3}}, 2, nil, nil, nil}
+	exp_node.right = &AVLNode{Elem{20, []ROWNUM{1}}, 1, exp_node, nil, nil}
+	exp_node.left = &AVLNode{Elem{5, []ROWNUM{2}}, 1, exp_node, nil, nil}
+	if exp_row != act_row {
+		t.Errorf("expect: %v, actual: %v", exp_row, act_row)
+	}
+	if !act_node.Equals(exp_node) {
+		t.Errorf("\nexpect: %v, \nactual: %v", exp_node, act_node)
+	}
+	act_cnt, exp_cnt = 3, tree.DataCount()
+	if act_cnt != exp_cnt {
+		t.Errorf("\nexpect: %v, \nactual: %v", exp_cnt, act_cnt)
+	}
 }
 
